17679b5a34efac7245c2f97ff6eff990eeefe66e
KryptonCaptain <cjalcorn.s446@gmail.com>
2018-01-27 11:22:48 +1100

    Extra magma block behaviour
    Magma block has random chance to vaporise water above it if only one layer deep, and will produce steam particles. If two layers of water or more, it will produce a stream of bubbles.

--- src/main/java/ganymedes01/etfuturum/blocks/MagmaBlock.java
+++ src/main/java/ganymedes01/etfuturum/blocks/MagmaBlock.java
@@ -5,14 +5,20 @@ import ganymedes01.etfuturum.IConfigurable;
 import ganymedes01.etfuturum.ModBlocks;
 import ganymedes01.etfuturum.configuration.ConfigurationHandler;
 import ganymedes01.etfuturum.core.utils.Utils;
+
+import java.util.Random;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class MagmaBlock extends Block implements IConfigurable{
 	
@@ -53,6 +59,42 @@ public class MagmaBlock extends Block implements IConfigurable{
 			return true;
 		}
 		return false;
+	}
+	
+	public void updateTick(World world, int x, int y, int z, Random rand) {
+		
+		Block block1 = world.getBlock(x, y+1, z);
+		Block block2 = world.getBlock(x, y+2, z);
+		
+		if ( (block1 == Blocks.water || block1 == Blocks.flowing_water) && block2.isAir(world, x, y+2, z)) {
+			world.setBlockToAir(x, y+1, z);
+			//world.playSound((double)((float)x + 0.5F), (double)((float)y + 0.5F), (double)((float)z + 0.5F), "random.fizz", 1.0F + rand.nextFloat(), rand.nextFloat() * 0.7F + 0.3F, false);
+			world.playSoundEffect((double)x + 0.5D, (double)y + 0.5D, (double)z + 0.5D, "random.fizz", 0.3F, 0.6F);
+			
+			/*
+			if (world instanceof WorldServer) { //MC 1.12 code
+				((WorldServer)world).spawnParticle("largesmoke", (double)x + 0.5D, (double)y + 0.25D, (double)z + 0.5D, 0.0D, 0.5D, 0.0D);
+			}*/
+		}
+		
+		
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void randomDisplayTick(World world, int x, int y, int z, Random rand) {
+		Block block1 = world.getBlock(x, y+1, z);
+		Block block2 = world.getBlock(x, y+2, z);
+		
+		if ( (block1 == Blocks.water || block1 == Blocks.flowing_water) && block2.isAir(world, x, y+2, z)) {
+			world.spawnParticle("explode", (double)x + 0.5D, (double)y + 1.0D, (double)z + 0.5D, 0.0D, 0.0D, 0.0D);
+		}
+		
+		else if ( (block1 == Blocks.water || block1 == Blocks.flowing_water) && 
+				(block2 == Blocks.water || block2 == Blocks.flowing_water) ) {
+			world.spawnParticle("bubble", (double)x + 0.5D, (double)y + 1.1D, (double)z + 0.5D, 0.0D, 1.0D, 0.0D);
+		}
+		
 	}
 
 }
