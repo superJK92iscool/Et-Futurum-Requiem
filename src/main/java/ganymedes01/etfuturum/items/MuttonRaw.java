793f31526fbd3f09c163bbea759ce7b5d0dd1cce
jss2a98aj <jsimmons3959@neo.rr.com>
2021-01-10 22:07:26 -0500

    Add the IConfigurable interface to Raw Mutton so it can be properly disabled

--- src/main/java/ganymedes01/etfuturum/items/MuttonRaw.java
+++ src/main/java/ganymedes01/etfuturum/items/MuttonRaw.java
@@ -1,16 +1,22 @@
 package ganymedes01.etfuturum.items;
 
 import ganymedes01.etfuturum.EtFuturum;
+import ganymedes01.etfuturum.IConfigurable;
 import ganymedes01.etfuturum.configuration.ConfigurationHandler;
 import ganymedes01.etfuturum.core.utils.Utils;
 import net.minecraft.item.ItemFood;
 
-public class MuttonRaw extends ItemFood {
+public class MuttonRaw extends ItemFood  implements IConfigurable{
 
     public MuttonRaw() {
         super(2, 0.3F, true);
         setTextureName("mutton_raw");
         setUnlocalizedName(Utils.getUnlocalisedName("mutton_raw"));
         setCreativeTab(ConfigurationHandler.enableMutton ? EtFuturum.creativeTab : null);
+    }
+    
+    @Override
+    public boolean isEnabled() {
+        return ConfigurationHandler.enableMutton;
     }
 }
