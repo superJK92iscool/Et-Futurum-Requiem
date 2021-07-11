84d0ada8aee35e8b5fac530ad4efc241968533df
Roadhog360 <Roadhog360@outlook.com>
2021-07-11 17:57:00 -0400

    Add logo, move mod info to code.

--- src/main/java/ganymedes01/etfuturum/lib/Reference.java
+++ src/main/java/ganymedes01/etfuturum/lib/Reference.java
@@ -3,9 +3,12 @@ package ganymedes01.etfuturum.lib;
 public class Reference {
 
 	public static final String MOD_ID = "etfuturum";
-	public static final String MOD_NAME = "Et Futurum";
+	public static final String MOD_NAME = "Et Futurum Requiem";
+	public static final String MOD_URL = "https://www.curseforge.com/minecraft/mc-mods/et-futurum-requiem";
 	public static final String DEPENDENCIES = "required-after:Forge@[10.13.4.1558,);after:Thaumcraft@[4.2.3.5,);after:TwilightForest;";
-	public static final String VERSION_NUMBER = "2.2.0";
+	public static final String VERSION_NUMBER = "2.2.1";
+	
+	
 	public static final String ITEM_BLOCK_TEXTURE_PATH = MOD_ID + ":";
 	public static final String ARMOUR_TEXTURE_PATH = ITEM_BLOCK_TEXTURE_PATH + "textures/models/armor/";
 	public static final String ENTITY_TEXTURE_PATH = ITEM_BLOCK_TEXTURE_PATH + "textures/entities/";
