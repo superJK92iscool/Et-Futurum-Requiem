05b36e511287d7e8fd28d9c23731fd0a954ebafe
makamys <makamys@outlook.com>
2021-09-09 18:48:18 +0200

    Embed Mixin, remove SpongeMixins dependency
    This seemed to not work properly for me until I ran a `./gradlew clean`

--- src/main/java/ganymedes01/etfuturum/lib/Reference.java
+++ src/main/java/ganymedes01/etfuturum/lib/Reference.java
@@ -5,7 +5,7 @@ public class Reference {
 	public static final String MOD_ID = "etfuturum";
 	public static final String MOD_NAME = "Et Futurum Requiem";
 	public static final String MOD_URL = "https://www.curseforge.com/minecraft/mc-mods/et-futurum-requiem";
-	public static final String DEPENDENCIES = "required-after:Forge@[10.13.4.1558,);required-after:spongemixins@[1.2.0,);after:Thaumcraft@[4.2.3.5,);after:TwilightForest;after:HardcoreEnderExpansion;";
+	public static final String DEPENDENCIES = "required-after:Forge@[10.13.4.1558,);after:Thaumcraft@[4.2.3.5,);after:TwilightForest;after:HardcoreEnderExpansion;";
 	public static final String VERSION_NUMBER = "2.2.2";
 	
 	
