5345c53c72645b0b4c499bc54e294575ffd1467d
Gregorius Techneticies <gregoriustech@gmail.com>
2021-11-12 17:26:53 +0100

    apparently NEI needs a proper Constructor for this to work

--- src/main/java/ganymedes01/etfuturum/NEI_EtFuturum_Config.java
+++ src/main/java/ganymedes01/etfuturum/NEI_EtFuturum_Config.java
@@ -21,111 +21,14 @@ public class NEI_EtFuturum_Config implements codechicken.nei.api.IConfigureNEI {
 	@Override
 	public void loadConfig() {
 		FurnaceRecipeHandler
-		// ======
 		// Smoker
-		// ======
-		handler = new FurnaceRecipeHandler() {
-			@Override
-			public void loadTransferRects() {
-				// Location of the "Recipes" Button on the Progress Bar of the Furnace.
-				transferRects.add(new RecipeTransferRect(new Rectangle(74, 23, 24, 18), getOverlayIdentifier()));
-				// Location of the "Fuel" Button on the Fuel Bar of the Furnace.
-				transferRects.add(new RecipeTransferRect(new Rectangle(50, 23, 18, 18), "fuel"));
-			}
-			@Override
-			public Class<? extends GuiContainer> getGuiClass() {
-				return GuiSmoker.class;
-			}
-			@Override
-			public String getRecipeName() {
-				return "Smoker"; // TODO insert actually localized Name of the Smoker here, so that this works with Language Packs.
-			}
-			@Override
-			public void loadCraftingRecipes(String outputId, Object... results) {
-				if (outputId.equals(getOverlayIdentifier())) {
-					Map<ItemStack, ItemStack> recipes = SmokerRecipes.smelting().getSmeltingList();
-					for (Entry<ItemStack, ItemStack> recipe : recipes.entrySet())
-						arecipes.add(new SmeltingPair(recipe.getKey(), recipe.getValue()));
-				} else
-					super.loadCraftingRecipes(outputId, results);
-			}
-			@Override
-			public void loadCraftingRecipes(ItemStack result) {
-				Map<ItemStack, ItemStack> recipes = SmokerRecipes.smelting().getSmeltingList();
-				for (Entry<ItemStack, ItemStack> recipe : recipes.entrySet())
-					if (NEIServerUtils.areStacksSameType(recipe.getValue(), result))
-						arecipes.add(new SmeltingPair(recipe.getKey(), recipe.getValue()));
-			}
-			@Override
-			public void loadUsageRecipes(ItemStack ingredient) {
-				Map<ItemStack, ItemStack> recipes = SmokerRecipes.smelting().getSmeltingList();
-				for (Entry<ItemStack, ItemStack> recipe : recipes.entrySet())
-					if (NEIServerUtils.areStacksSameTypeCrafting(recipe.getKey(), ingredient)) {
-						SmeltingPair arecipe = new SmeltingPair(recipe.getKey(), recipe.getValue());
-						//arecipe.setIngredientPermutation(Arrays.asList(arecipe.ingred), ingredient);
-						arecipes.add(arecipe);
-					}
-			}
-			@Override
-			public String getOverlayIdentifier() {
-				return "etfuturum.smoker";
-			}
-		};
+		handler = new NEI_Recipes_Smoker();
 		// make it possible to use the "R" Key
 		GuiCraftingRecipe.craftinghandlers.add(handler);
 		// make it possible to use the "U" Key
 		GuiUsageRecipe.usagehandlers.add(handler);
-		
-		// =============
 		// Blast Furnace
-		// =============
-		handler = new FurnaceRecipeHandler() {
-			@Override
-			public void loadTransferRects() {
-				// Location of the "Recipes" Button on the Progress Bar of the Furnace.
-				transferRects.add(new RecipeTransferRect(new Rectangle(74, 23, 24, 18), getOverlayIdentifier()));
-				// Location of the "Fuel" Button on the Fuel Bar of the Furnace.
-				transferRects.add(new RecipeTransferRect(new Rectangle(50, 23, 18, 18), "fuel"));
-			}
-			@Override
-			public Class<? extends GuiContainer> getGuiClass() {
-				return GuiBlastFurnace.class;
-			}
-			@Override
-			public String getRecipeName() {
-				return "Blast Furnace"; // TODO insert actually localized Name of the Blast Furnace here, so that this works with Language Packs.
-			}
-			@Override
-			public void loadCraftingRecipes(String outputId, Object... results) {
-				if (outputId.equals(getOverlayIdentifier())) {
-					Map<ItemStack, ItemStack> recipes = BlastFurnaceRecipes.smelting().getSmeltingList();
-					for (Entry<ItemStack, ItemStack> recipe : recipes.entrySet())
-						arecipes.add(new SmeltingPair(recipe.getKey(), recipe.getValue()));
-				} else
-					super.loadCraftingRecipes(outputId, results);
-			}
-			@Override
-			public void loadCraftingRecipes(ItemStack result) {
-				Map<ItemStack, ItemStack> recipes = BlastFurnaceRecipes.smelting().getSmeltingList();
-				for (Entry<ItemStack, ItemStack> recipe : recipes.entrySet())
-					if (NEIServerUtils.areStacksSameType(recipe.getValue(), result))
-						arecipes.add(new SmeltingPair(recipe.getKey(), recipe.getValue()));
-			}
-			@Override
-			public void loadUsageRecipes(ItemStack ingredient) {
-				Map<ItemStack, ItemStack> recipes = BlastFurnaceRecipes.smelting().getSmeltingList();
-				for (Entry<ItemStack, ItemStack> recipe : recipes.entrySet())
-					if (NEIServerUtils.areStacksSameTypeCrafting(recipe.getKey(), ingredient)) {
-						SmeltingPair arecipe = new SmeltingPair(recipe.getKey(), recipe.getValue());
-						//arecipe.setIngredientPermutation(Arrays.asList(arecipe.ingred), ingredient);
-						arecipes.add(arecipe);
-					}
-			}
-			@Override
-			public String getOverlayIdentifier() {
-				return "etfuturum.blastfurnace";
-			}
-		};
+		handler = new NEI_Recipes_BlastFurnace();
 		// make it possible to use the "R" Key
 		GuiCraftingRecipe.craftinghandlers.add(handler);
 		// make it possible to use the "U" Key
@@ -138,5 +41,91 @@ public class NEI_EtFuturum_Config implements codechicken.nei.api.IConfigureNEI {
 	@Override
 	public String getVersion() {
 		return Reference.VERSION_NUMBER;
+	}
+	
+	public static class NEI_Recipes_Smoker extends FurnaceRecipeHandler {
+		public NEI_Recipes_Smoker() {}
+		
+		@Override
+		public void loadTransferRects() {
+			// Location of the "Recipes" Button on the Progress Bar of the Furnace.
+			transferRects.add(new RecipeTransferRect(new Rectangle(74, 23, 24, 18), getOverlayIdentifier()));
+			// Location of the "Fuel" Button on the Fuel Bar of the Furnace.
+			transferRects.add(new RecipeTransferRect(new Rectangle(50, 23, 18, 18), "fuel"));
+		}
+		@Override
+		public void loadCraftingRecipes(String outputId, Object... results) {
+			if (outputId.equals(getOverlayIdentifier())) {
+				Map<ItemStack, ItemStack> recipes = SmokerRecipes.smelting().getSmeltingList();
+				for (Entry<ItemStack, ItemStack> recipe : recipes.entrySet())
+					arecipes.add(new SmeltingPair(recipe.getKey(), recipe.getValue()));
+			} else
+				super.loadCraftingRecipes(outputId, results);
+		}
+		@Override
+		public void loadCraftingRecipes(ItemStack result) {
+			Map<ItemStack, ItemStack> recipes = SmokerRecipes.smelting().getSmeltingList();
+			for (Entry<ItemStack, ItemStack> recipe : recipes.entrySet())
+				if (NEIServerUtils.areStacksSameType(recipe.getValue(), result))
+					arecipes.add(new SmeltingPair(recipe.getKey(), recipe.getValue()));
+		}
+		@Override
+		public void loadUsageRecipes(ItemStack ingredient) {
+			Map<ItemStack, ItemStack> recipes = SmokerRecipes.smelting().getSmeltingList();
+			for (Entry<ItemStack, ItemStack> recipe : recipes.entrySet())
+				if (NEIServerUtils.areStacksSameTypeCrafting(recipe.getKey(), ingredient)) {
+					SmeltingPair arecipe = new SmeltingPair(recipe.getKey(), recipe.getValue());
+					//arecipe.setIngredientPermutation(Arrays.asList(arecipe.ingred), ingredient);
+					arecipes.add(arecipe);
+				}
+		}
+		
+		// TODO insert actually localized Name of the Smoker here, so that this works with Language Packs.
+		@Override public String getRecipeName() {return "Smoker";}
+		@Override public String getOverlayIdentifier() {return "etfuturum.smoker";}
+		@Override public Class<? extends GuiContainer> getGuiClass() {return GuiSmoker.class;}
+	}
+	
+	public static class NEI_Recipes_BlastFurnace extends FurnaceRecipeHandler {
+		public NEI_Recipes_BlastFurnace() {}
+		
+		@Override
+		public void loadTransferRects() {
+			// Location of the "Recipes" Button on the Progress Bar of the Furnace.
+			transferRects.add(new RecipeTransferRect(new Rectangle(74, 23, 24, 18), getOverlayIdentifier()));
+			// Location of the "Fuel" Button on the Fuel Bar of the Furnace.
+			transferRects.add(new RecipeTransferRect(new Rectangle(50, 23, 18, 18), "fuel"));
+		}
+		@Override
+		public void loadCraftingRecipes(String outputId, Object... results) {
+			if (outputId.equals(getOverlayIdentifier())) {
+				Map<ItemStack, ItemStack> recipes = BlastFurnaceRecipes.smelting().getSmeltingList();
+				for (Entry<ItemStack, ItemStack> recipe : recipes.entrySet())
+					arecipes.add(new SmeltingPair(recipe.getKey(), recipe.getValue()));
+			} else
+				super.loadCraftingRecipes(outputId, results);
+		}
+		@Override
+		public void loadCraftingRecipes(ItemStack result) {
+			Map<ItemStack, ItemStack> recipes = BlastFurnaceRecipes.smelting().getSmeltingList();
+			for (Entry<ItemStack, ItemStack> recipe : recipes.entrySet())
+				if (NEIServerUtils.areStacksSameType(recipe.getValue(), result))
+					arecipes.add(new SmeltingPair(recipe.getKey(), recipe.getValue()));
+		}
+		@Override
+		public void loadUsageRecipes(ItemStack ingredient) {
+			Map<ItemStack, ItemStack> recipes = BlastFurnaceRecipes.smelting().getSmeltingList();
+			for (Entry<ItemStack, ItemStack> recipe : recipes.entrySet())
+				if (NEIServerUtils.areStacksSameTypeCrafting(recipe.getKey(), ingredient)) {
+					SmeltingPair arecipe = new SmeltingPair(recipe.getKey(), recipe.getValue());
+					//arecipe.setIngredientPermutation(Arrays.asList(arecipe.ingred), ingredient);
+					arecipes.add(arecipe);
+				}
+		}
+		
+		// TODO insert actually localized Name of the Blast Furnace here, so that this works with Language Packs.
+		@Override public String getRecipeName() {return "Blast Furnace";}
+		@Override public String getOverlayIdentifier() {return "etfuturum.blastfurnace";}
+		@Override public Class<? extends GuiContainer> getGuiClass() {return GuiBlastFurnace.class;}
 	}
 }
