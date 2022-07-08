0a0e6283e30ea01784c153a34e806fca05c962e8
makamys <makamys@outlook.com>
2022-07-08 21:43:29 +0200

    Clean up GuiShulkerBox

--- src/main/java/ganymedes01/etfuturum/client/gui/inventory/GuiShulkerBox.java
+++ src/main/java/ganymedes01/etfuturum/client/gui/inventory/GuiShulkerBox.java
@@ -14,9 +14,8 @@ import net.minecraft.util.ResourceLocation;
 
 public class GuiShulkerBox extends GuiContainer {
 	
-	private static final ResourceLocation backgroundVanilla = new ResourceLocation("textures/gui/container/generic_54.png");
 	private static final ResourceLocation[] backgrounds = Arrays.stream(TileEntityShulkerBox.ShulkerBoxType.values())
-			.map(t -> t.getGuiTextureName() == null ? null : 
+			.map(t -> t.getGuiTextureName() == null ? new ResourceLocation("textures/gui/container/generic_54.png") : 
 				new ResourceLocation(String.format("etfuturum:textures/gui/container/ironshulkerbox/%s.png", t.getGuiTextureName())))
 			.toArray(ResourceLocation[]::new);
 	private IInventory upperChestInventory;
@@ -24,16 +23,18 @@ public class GuiShulkerBox extends GuiContainer {
 	/** window height is calculated with these values; the more rows, the heigher */
 	private int inventoryRows;
 	
-	public GuiShulkerBox(IInventory p_i1083_1_, IInventory p_i1083_2_) {
-		super(new ContainerChestGeneric(p_i1083_1_, p_i1083_2_, ((TileEntityShulkerBox)p_i1083_2_).getRowSize(), ((TileEntityShulkerBox)p_i1083_2_).getSizeInventory() != 27));
-		this.upperChestInventory = p_i1083_1_;
-		this.lowerChestInventory = p_i1083_2_;
+	private TileEntityShulkerBox.ShulkerBoxType type;
+	
+	public GuiShulkerBox(IInventory playerInventory, IInventory chestInventory) {
+		super(new ContainerChestGeneric(playerInventory, chestInventory, ((TileEntityShulkerBox)chestInventory).getRowSize(), ((TileEntityShulkerBox)chestInventory).getSizeInventory() != 27));
+		this.upperChestInventory = playerInventory;
+		this.lowerChestInventory = chestInventory;
 		
-		ShulkerBoxType type = ((TileEntityShulkerBox)p_i1083_2_).type;
+		this.type = ((TileEntityShulkerBox)chestInventory).type;
 		this.xSize = type.getXSize();
 		this.ySize = type.getYSize();
 		this.allowUserInput = false;
-		this.inventoryRows = p_i1083_2_.getSizeInventory() / type.getRowSize();
+		this.inventoryRows = chestInventory.getSizeInventory() / type.getRowSize();
 	}
 
 	/**
@@ -41,7 +42,6 @@ public class GuiShulkerBox extends GuiContainer {
 	 */
 	protected void drawGuiContainerForegroundLayer(int p_146979_1_, int p_146979_2_)
 	{
-		ShulkerBoxType type = ((TileEntityShulkerBox)lowerChestInventory).type;
 		if(type == ShulkerBoxType.VANILLA) {
 			this.fontRendererObj.drawString(this.lowerChestInventory.hasCustomInventoryName() ? this.lowerChestInventory.getInventoryName() : I18n.format(this.lowerChestInventory.getInventoryName(), new Object[0]), 8, 6, 4210752);
 			this.fontRendererObj.drawString(this.upperChestInventory.hasCustomInventoryName() ? this.upperChestInventory.getInventoryName() : I18n.format(this.upperChestInventory.getInventoryName(), new Object[0]), 8, this.ySize - 96 + 2, 4210752);
@@ -50,19 +50,15 @@ public class GuiShulkerBox extends GuiContainer {
 
 	protected void drawGuiContainerBackgroundLayer(float p_146976_1_, int p_146976_2_, int p_146976_3_)
 	{
-		ShulkerBoxType type = ((TileEntityShulkerBox)lowerChestInventory).type;
-		
 		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+		this.mc.getTextureManager().bindTexture(backgrounds[type.ordinal()]);
+		int k = (this.width - this.xSize) / 2;
+		int l = (this.height - this.ySize) / 2;
+		
 		if(type == ShulkerBoxType.VANILLA) {
-			this.mc.getTextureManager().bindTexture(backgroundVanilla);
-			int k = (this.width - this.xSize) / 2;
-			int l = (this.height - this.ySize) / 2;
 			this.drawTexturedModalRect(k, l, 0, 0, this.xSize, this.inventoryRows * 18 + 17);
 	        this.drawTexturedModalRect(k, l + this.inventoryRows * 18 + 17, 0, 126, this.xSize, 96);
 		} else {
-			this.mc.getTextureManager().bindTexture(backgrounds[type.ordinal()]);
-			int k = (this.width - this.xSize) / 2;
-			int l = (this.height - this.ySize) / 2;
 			this.drawTexturedModalRect(k, l, 0, 0, this.xSize, this.ySize);
 		}
 	}
