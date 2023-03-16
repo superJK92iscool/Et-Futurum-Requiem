59bcca518c4e778bf39a31a9764208a45abad37d
jss2a98aj <jss2a98aj@gmail.com>
2023-03-16 00:18:44 -0400

    Add missing final annotation to furnace mixin

--- src/main/java/ganymedes01/etfuturum/mixins/client/MixinBlockFurnace.java
+++ src/main/java/ganymedes01/etfuturum/mixins/client/MixinBlockFurnace.java
@@ -2,6 +2,7 @@ package ganymedes01.etfuturum.mixins.client;
 
 import java.util.Random;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,6 +17,7 @@ import net.minecraft.world.World;
 public class MixinBlockFurnace {
 	
 	@Shadow
+	@Final
 	private boolean field_149932_b;
 
 	@Inject(method = "randomDisplayTick(Lnet/minecraft/world/World;IIILjava/util/Random;)V", at = @At(value = "HEAD"))
