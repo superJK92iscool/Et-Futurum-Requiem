14227fdd4b1c9e2257fe4d0b46baa3eb9acf5c35
Yoghurt4C <yoghurt4242@gmail.com>
2023-04-20 16:59:45 +0300

    mistä tunnistat ekstrovertin suomalaisen?
    kun hän puhuu sinulle, hän tuijottaa sinun jalkojasi eikä omiaan

--- src/main/java/ganymedes01/etfuturum/compat/nei/ComposterHandler.java
+++ src/main/java/ganymedes01/etfuturum/compat/nei/ComposterHandler.java
@@ -19,73 +19,26 @@ import net.minecraft.util.WeightedRandomChestContent;
 import org.lwjgl.opengl.GL11;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import static codechicken.lib.gui.GuiDraw.*;
 
 public class ComposterHandler extends TemplateRecipeHandler implements ICraftingHandler, IUsageHandler {
-    public final PositionedStack compooster = new PositionedStack(new ItemStack(ModBlocks.composter), 75, 80);
+    public final PositionedStack compooster = new PositionedStack(new ItemStack(ModBlocks.composter), 75, 78, false);
+    public final List<PositionedStack> outputs = new ArrayList<>();
+    public final ItemStackMap<TableProps> props = new ItemStackMap<>();
 
     public class CompostedRecipe extends CachedRecipe {
-        public final ItemStackMap<TableProps> props;
-
-        public CompostedRecipe() {
-            List<ItemStack> inputs = new ArrayList<>(CompostingRegistry.getComposts().keySet());
-
-            List<List<ItemStack>> stacks = new ArrayList<>();
-            for (int i = 0; i < Math.min(inputs.size(), 36); i++) {
-                List<ItemStack> stack = new ArrayList<>();
-                stack.add(inputs.get(i));
-                int k = i;
-                while (k + 36 < inputs.size()) {
-                    k += 36;
-                    stack.add(inputs.get(k));
-                }
-                stacks.add(stack);
-            }
-
-            List<PositionedStack> in = new ArrayList<>();
-            int i = 0, x = 3, y = 3;
-            for (List<ItemStack> stack : stacks) {
-                in.add(new PermutedStack(stack, x, y, stack.size() > 1));
-                x += 18;
-                i++;
-                if (i == 9) {
-                    i = 0;
-                    x = 3;
-                    y += 18;
-                }
-            }
-            this.inputs = in;
-
-            WeightedRandomChestContent[] tables = EtFuturumLootTables.COMPOSTER_LOOT.getItems(Minecraft.getMinecraft().thePlayer.worldObj.rand);
-            List<PositionedStack> out = new ArrayList<>();
-            this.props = new ItemStackMap<>();
-            int max = tables.length;
-            i = 0;
-            x = (166 - (18 * (max % 9))) >> 1;
-            y = 98;
-            for (WeightedRandomChestContent table : tables) {
-                out.add(new PositionedStack(table.theItemId, x, y, false));
-                this.props.put(table.theItemId, new TableProps(table.theMinimumChanceToGenerateItem, table.theMaximumChanceToGenerateItem, table.itemWeight));
-                x += 18;
-                i++;
-                if (i == 9) {
-                    max -= 9;
-                    x = (166 - (18 * (max % 9))) >> 1;
-                    y += 18;
-                    i = 0;
-                }
-            }
-            this.outputs = out;
+        public CompostedRecipe(List<PositionedStack> inputs) {
+            this.inputs = inputs;
         }
 
-        public List<PositionedStack> inputs;
-        public List<PositionedStack> outputs;
+        public final List<PositionedStack> inputs;
 
         @Override
         public List<PositionedStack> getIngredients() {
-            return getCycledIngredients(cycleticks / 48, inputs);
+            return inputs;
         }
 
         @Override
@@ -99,30 +52,96 @@ public class ComposterHandler extends TemplateRecipeHandler implements ICrafting
         }
     }
 
+    private void composeRecipeMap(ItemStack predicate) {
+        List<ItemStack> inputs = new ArrayList<>(CompostingRegistry.getComposts().keySet());
+        WeightedRandomChestContent[] tables = EtFuturumLootTables.COMPOSTER_LOOT.getItems(Minecraft.getMinecraft().thePlayer.worldObj.rand);
+        if (!itemCheck(predicate, inputs) && !tableCheck(predicate, tables) && !this.compooster.contains(predicate))
+            return;
+        arecipes.clear();
+        outputs.clear();
+        props.clear();
+
+        List<List<PositionedStack>> in = new ArrayList<>();
+        int i = inputs.size();
+        int x, y;
+        while (i > 0) {
+            List<PositionedStack> slots = new ArrayList<>();
+            int size = Math.min(i, 36);
+            x = 3;
+            y = 3;
+            for (int k = 0; k < size; ) {
+                slots.add(new PositionedStack(inputs.remove(0), x, y, false));
+                x += 18;
+                k++;
+                if (k % 9 == 0) {
+                    x = 3;
+                    y += 18;
+                }
+            }
+            in.add(slots);
+            i -= size;
+        }
+
+        int max = tables.length;
+        i = 0;
+        x = (168 - (18 * Math.min(max, 9))) >> 1;
+        y = (210 - (max / 9 * 18)) >> 1;
+        for (WeightedRandomChestContent table : tables) {
+            this.outputs.add(new PositionedStack(table.theItemId, x, y, false));
+            this.props.put(table.theItemId, new TableProps(table.theMinimumChanceToGenerateItem, table.theMaximumChanceToGenerateItem, table.itemWeight));
+            x += 18;
+            i++;
+            if (i == 9) {
+                max -= 9;
+                x = (168 - (18 * (max % 9))) >> 1;
+                y += 18;
+                i = 0;
+            }
+        }
+
+        for (List<PositionedStack> slots : in) {
+            arecipes.add(new CompostedRecipe(slots));
+        }
+    }
+
+    private boolean itemCheck(ItemStack stack, Collection<ItemStack> inputs) {
+        for (ItemStack input : inputs) {
+            if (NEIClientUtils.areStacksSameTypeCrafting(input, stack)) return true;
+        }
+        return false;
+    }
+
+    private boolean tableCheck(ItemStack stack, WeightedRandomChestContent[] table) {
+        for (WeightedRandomChestContent cont : table) {
+            if (NEIClientUtils.areStacksSameType(cont.theItemId, stack)) return true;
+        }
+        return false;
+    }
+
     @Override
     public void loadCraftingRecipes(String outputId, Object... results) {
-        if (outputId.equals(this.getOverlayIdentifier())) arecipes.add(new CompostedRecipe());
+        if (outputId.equals(this.getOverlayIdentifier())) composeRecipeMap(null);
         else loadCraftingRecipes((ItemStack) results[0]);
     }
 
     @Override
     public void loadCraftingRecipes(ItemStack result) {
         for (WeightedRandomChestContent item : EtFuturumLootTables.COMPOSTER_LOOT.getItems(Minecraft.getMinecraft().thePlayer.worldObj.rand)) {
-            if (NEIClientUtils.areStacksSameType(item.theItemId, result)) arecipes.add(new CompostedRecipe());
+            if (NEIClientUtils.areStacksSameType(item.theItemId, result)) composeRecipeMap(result);
         }
-        if (CompostingRegistry.getComposts().containsKey(result)) arecipes.add(new CompostedRecipe());
     }
 
     @Override
     public void loadUsageRecipes(String inputId, Object... ingredients) {
-        if (inputId.equals(this.getOverlayIdentifier())) arecipes.add(new CompostedRecipe());
+        if (inputId.equals(this.getOverlayIdentifier())) composeRecipeMap(null);
         else loadUsageRecipes((ItemStack) ingredients[0]);
     }
 
     @Override
     public void loadUsageRecipes(ItemStack ingredient) {
-        if (CompostingRegistry.getComposts().containsKey(ingredient) || this.compooster.contains(ingredient))
-            arecipes.add(new CompostedRecipe());
+        if (itemCheck(ingredient, CompostingRegistry.getComposts().keySet()) || this.compooster.contains(ingredient)) {
+            composeRecipeMap(ingredient);
+        }
     }
 
     @Override
@@ -138,19 +157,17 @@ public class ComposterHandler extends TemplateRecipeHandler implements ICrafting
         GL11.glPushMatrix();
         GL11.glScalef(0.5f, 0.5f, 1f);
         for (PositionedStack stack : cached.getIngredients()) {
-            PermutedStack perm = (PermutedStack) stack;
-            int chance = CompostingRegistry.getCompostChance(perm.items[perm.permutation]);
+            int chance = CompostingRegistry.getCompostChance(stack.items[0]);
             String str = chance + "%";
-            GuiDraw.drawString(str, (perm.relx + 16 - (fontRenderer.getStringWidth(str) >> 1)) << 1, (perm.rely + 12) << 1, 0xFFFFFF, true);
+            GuiDraw.drawString(str, (stack.relx + 16 - (fontRenderer.getStringWidth(str) >> 1)) << 1, (stack.rely + 12) << 1, 0xFFFFFF, true);
         }
         GL11.glPopMatrix();
     }
 
     @Override
     public List<String> handleItemTooltip(GuiRecipe gui, ItemStack stack, List<String> currenttip, int recipe) {
-        CompostedRecipe cached = (CompostedRecipe) arecipes.get(recipe);
-        if (stack != null && cached.props.containsKey(stack)) {
-            TableProps props = cached.props.get(stack);
+        if (stack != null && this.props.containsKey(stack)) {
+            TableProps props = this.props.get(stack);
             currenttip.add(currenttip.size() - 1, "§9" + I18n.format("efr.nei.amount") + ":§6 " + props.minSize + "§7~§6" + props.maxSize);
             currenttip.add(currenttip.size() - 1, "§9" + I18n.format("efr.nei.weight") + ":§6 " + props.wt);
         }
@@ -176,25 +193,6 @@ public class ComposterHandler extends TemplateRecipeHandler implements ICrafting
     public String getRecipeName() {
         return I18n.format("efr.nei.composter");
     }
-
-    /**
-     * Bogus class that captures the generated permutation for percentages
-     */
-    public static class PermutedStack extends PositionedStack {
-        public int permutation;
-
-        public PermutedStack(Object object, int x, int y, boolean genPerms) {
-            super(object, x, y, genPerms);
-            this.permutation = 0;
-        }
-
-        @Override
-        public void setPermutationToRender(int index) {
-            super.setPermutationToRender(index);
-            this.permutation = index;
-        }
-    }
-
 
     /**
      * Bogus class that captures loot table properties
