484a43e8e11d3b112220deab59386a8adb3b3037
Roadhog360 <Roadhog360@outlook.com>
2023-05-14 18:30:47 -0500

    Optimize fence connection code via IntelliJ suggestions

--- src/main/java/ganymedes01/etfuturum/mixins/fencewallconnect/MixinBlockFence.java
+++ src/main/java/ganymedes01/etfuturum/mixins/fencewallconnect/MixinBlockFence.java
@@ -16,6 +16,6 @@ public class MixinBlockFence {
 	public boolean canConnectFenceTo(IBlockAccess p_149826_1_, int p_149826_2_, int p_149826_3_, int p_149826_4_)
 	{
 		Block block = p_149826_1_.getBlock(p_149826_2_, p_149826_3_, p_149826_4_);
-		return !(block instanceof BlockFence && block.getMaterial() == ((BlockFence)(Object)this).blockMaterial) && !(block instanceof BlockFenceGate) ? (block.blockMaterial.isOpaque() && block.renderAsNormalBlock() ? block.blockMaterial != Material.gourd : false) : true;
+		return block instanceof BlockFence && block.getMaterial() == ((BlockFence) (Object) this).blockMaterial || block instanceof BlockFenceGate || (block.blockMaterial.isOpaque() && block.renderAsNormalBlock() && block.blockMaterial != Material.gourd);
 	}
 }
--- src/main/java/ganymedes01/etfuturum/mixins/fencewallconnect/MixinBlockWall.java
+++ src/main/java/ganymedes01/etfuturum/mixins/fencewallconnect/MixinBlockWall.java
@@ -16,6 +16,6 @@ public class MixinBlockWall {
 	public boolean canConnectWallTo(IBlockAccess p_150091_1_, int p_150091_2_, int p_150091_3_, int p_150091_4_)
 	{
 		Block block = p_150091_1_.getBlock(p_150091_2_, p_150091_3_, p_150091_4_);
-		return !(block instanceof BlockWall && block.getMaterial() == ((BlockWall)(Object)this).blockMaterial) && !(block instanceof BlockFenceGate) ? (block.blockMaterial.isOpaque() && block.renderAsNormalBlock() ? block.blockMaterial != Material.gourd : false) : true;
+		return block instanceof BlockWall && block.getMaterial() == ((BlockWall) (Object) this).blockMaterial || block instanceof BlockFenceGate || (block.blockMaterial.isOpaque() && block.renderAsNormalBlock() && block.blockMaterial != Material.gourd);
 	}
 }
