f3c54ab26f71eb9fb6a1f38bf8582945c4ad83f4
Roadhog360 <Roadhog360@outlook.com>
2023-05-14 16:51:41 -0500

    Remove unused ExternalContent fields

--- src/main/java/ganymedes01/etfuturum/core/utils/ExternalContent.java
+++ src/main/java/ganymedes01/etfuturum/core/utils/ExternalContent.java
@@ -4,32 +4,16 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 
+import javax.naming.ldap.ExtendedRequest;
+import java.util.function.Supplier;
+
 public class ExternalContent {
 
-	public static final Item np_netherite_ingot = GameRegistry.findItem("netheriteplus", "NetheriteIngot");
-	public static final Item np_netherite_helmet = GameRegistry.findItem("netheriteplus", "NetheriteHelm");
-	public static final Item np_netherite_chestplate = GameRegistry.findItem("netheriteplus", "NetheriteChest");
-	public static final Item np_netherite_leggings = GameRegistry.findItem("netheriteplus", "NetheriteLegg");
-	public static final Item np_netherite_boots = GameRegistry.findItem("netheriteplus", "NetheriteBoots");
-	
-	public static final Item np_netherite_sword = GameRegistry.findItem("netheriteplus", "ItemNetheriteSword");
-	public static final Item np_netherite_hoe = GameRegistry.findItem("netheriteplus", "ItemNetheriteHoe");
-	public static final Item np_netherite_axe = GameRegistry.findItem("netheriteplus", "ItemNetheriteAxe");
-	public static final Item np_netherite_pickaxe = GameRegistry.findItem("netheriteplus", "NetheritePickaxe");
-	public static final Item np_netherite_shovel = GameRegistry.findItem("netheriteplus", "ItemNetheriteShovel");
-	
 	public static final Block enderlicious_end_rock = GameRegistry.findBlock("enderlicious", "EndRock");
 	public static final Block hee_end_stone = GameRegistry.findBlock("HardcoreEnderExpansion", "end_stone_terrain");
-	
+
 	public static final Block enderlicious_sand = GameRegistry.findBlock("enderlicious", "EndSand");
 	
-	public static final Block netherlicious_bone_block = GameRegistry.findBlock("netherlicious", "BoneBlock");
-	public static final Block utd_bone_block = GameRegistry.findBlock("uptodate", "bone_block");
-	
 	public static final Block netherlicious_basalt_bricks = GameRegistry.findBlock("netherlicious", "BasaltBricks");
-
-	public static final Item ic2_electric_hoe = GameRegistry.findItem("IC2", "itemToolHoe");
-
-	public static final Item thaumcraft_hoe_of_growth = GameRegistry.findItem("Thaumcraft", "ItemHoeElemental");
 
 }
