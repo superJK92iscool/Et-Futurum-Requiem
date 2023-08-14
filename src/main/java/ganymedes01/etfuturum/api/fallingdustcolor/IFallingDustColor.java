1cc9fcad9db37ce7383ae427d896942fd9c7c2b9
Roadhog360 <Roadhog360@outlook.com>
2023-08-14 00:58:04 -0500

    Add comment for IFallingDustColor [ci skip]

--- src/main/java/ganymedes01/etfuturum/api/fallingdustcolor/IFallingDustColor.java
+++ src/main/java/ganymedes01/etfuturum/api/fallingdustcolor/IFallingDustColor.java
@@ -1,5 +1,14 @@
 package ganymedes01.etfuturum.api.fallingdustcolor;
 
 public interface IFallingDustColor {
+	/**
+	 * If falling dust colors are enabled in mixin.cfg, what color should this BlockFalling's dust be?
+	 * Note to mod authors: You can simply add this function to your BlockFalling to specify a dust color.
+	 * You don't need to add the interface if you do not wish to; Your IDE may falsely declare the function as unused however.
+	 * If this function is not overridden by a BlockFalling, it will use the block's MapColor
+	 *
+	 * @param meta
+	 * @return An ARGB color value, typically hex. EG 0xFFAA00FF for purple dust
+	 */
 	int getDustColor(int meta);
 }
