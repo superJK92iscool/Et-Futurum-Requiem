807a960c2b5b861e387d8540edda4857f8c2c1f4
Roadhog360 <Roadhog360@outlook.com>
2023-09-15 18:43:48 -0500

    Fix blue ice not being solid

--- src/main/java/ganymedes01/etfuturum/blocks/BlockBlueIce.java
+++ src/main/java/ganymedes01/etfuturum/blocks/BlockBlueIce.java
@@ -2,15 +2,12 @@ package ganymedes01.etfuturum.blocks;
 
 import ganymedes01.etfuturum.EtFuturum;
 import ganymedes01.etfuturum.core.utils.Utils;
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
+import net.minecraft.block.BlockPackedIce;
 
-import java.util.Random;
-
-public class BlockBlueIce extends Block {
+public class BlockBlueIce extends BlockPackedIce {
 
 	public BlockBlueIce() {
-		super(Material.ice);
+		super();
 		this.slipperiness = 0.989F;
 		this.setHarvestLevel("pickaxe", 0);
 		this.setStepSound(soundTypeGlass);
@@ -20,10 +17,4 @@ public class BlockBlueIce extends Block {
 		this.setBlockTextureName("blue_ice");
 		this.setCreativeTab(EtFuturum.creativeTabBlocks);
 	}
-
-	@Override
-	public int quantityDropped(Random p_149745_1_) {
-		return 0;
-	}
-
 }
