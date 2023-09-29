216b3e3ac5459a2afac50ce5520ae4a617402a5d
Kam <KamronGaming@gmail.com>
2023-09-29 18:08:14 -0400

    Fix Light Block AI Pathing

--- src/main/java/ganymedes01/etfuturum/blocks/BlockLight.java
+++ src/main/java/ganymedes01/etfuturum/blocks/BlockLight.java
@@ -111,4 +111,10 @@ public class BlockLight extends BlockBarrier implements ISubBlocksBlock {
 	public boolean isNormalCube() {
 		return false;
 	}
+
+	@Override
+	public boolean getBlocksMovement(IBlockAccess p_149655_1_, int p_149655_2_, int p_149655_3_, int p_149655_4_)
+	{
+		return true;
+	}
 }
