68f33b52e8a19ae9b547cf2dc3113156eda8c886
Roadhog360 <Roadhog360@outlook.com>
2023-12-03 01:21:57 -0600

    Fix azalea crossed squares getting wrong colors Put it to render after the first cube so it inherits the coloration of that instead.

--- src/main/java/ganymedes01/etfuturum/client/renderer/block/BlockAzaleaRenderer.java
+++ src/main/java/ganymedes01/etfuturum/client/renderer/block/BlockAzaleaRenderer.java
@@ -31,12 +31,12 @@ public class BlockAzaleaRenderer extends BlockModelBase {
 
 	@Override
 	public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, Block block, int modelId, RenderBlocks renderer) {
-		//We don't need to cull the bottom (0) face because we do that in
-		renderer.drawCrossedSquares(block.getBlockTextureFromSide(0), x, y, z, 1.0F);
-		boolean prev = renderer.renderAllFaces;
+		//We don't need to cull the bottom (0) face because we do that in the block class
+		boolean prev = renderer.renderFromInside;
 		renderer.renderFromInside = true;
 		renderStandardWorldCube(world, x, y, z, block, modelId, renderer, 0, 0, 0, 1, 1, 1);
 		renderer.renderFromInside = prev;
+		renderer.drawCrossedSquares(block.getBlockTextureFromSide(0), x, y, z, 1.0F);
 		return renderStandardWorldCube(world, x, y, z, block, modelId, renderer, 0, 0, 0, 1, 1, 1);
 	}
 
