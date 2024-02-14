e9b1ca15578ddd302dfd3bf1ba7b44d1d7c768ca
Roadhog360 <Roadhog360@outlook.com>
2024-02-14 13:43:29 -0600

    Revert light level 0 behavior

--- src/main/java/ganymedes01/etfuturum/core/handlers/ServerEventHandler.java
+++ src/main/java/ganymedes01/etfuturum/core/handlers/ServerEventHandler.java
@@ -1,7 +1,6 @@
 package ganymedes01.etfuturum.core.handlers;
 
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.eventhandler.Event.Result;
 import cpw.mods.fml.common.eventhandler.EventPriority;
@@ -76,7 +75,6 @@ import net.minecraft.util.MovingObjectPosition.MovingObjectType;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldProviderHell;
 import net.minecraft.world.WorldServer;
-import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.ChunkProviderServer;
@@ -1360,13 +1358,6 @@ public class ServerEventHandler {
 			int y = MathHelper.floor_double(event.y);
 			int z = MathHelper.floor_double(event.z);
 
-			if (ConfigEntities.enableLightLevel0 && (event.entityLiving instanceof IMob || getSpawnTypes(event.entityLiving).contains(EnumCreatureType.monster))) {
-				if (event.entityLiving.worldObj.getBlockLightValue(x, y, z) > 0) {
-					event.setResult(Result.DENY);
-					return;
-				}
-			}
-
 			if (event.world.provider instanceof WorldProviderHell) {
 				if (world.getBlock(x, y - 1, z) == ModBlocks.NETHER_WART.get() && world.getBlockMetadata(x, y - 1, z) == 0) {
 					if (!(event.entity instanceof EntityFlying)) {
@@ -1375,39 +1366,6 @@ public class ServerEventHandler {
 				}
 			}
 		}
-	}
-
-	private static final Map<Class, List<EnumCreatureType>> typesMap = Maps.newHashMap();
-
-	@SuppressWarnings("unchecked")
-	/**
-	 * Checks the spawn types the mob comes from. Used by the light level 0 monster spawning in case the monster isn't instance of IMob but is still being spawned as one.
-	 * Derived from CoreTweaks
-	 * @author makamys
-	 */
-	private static List<EnumCreatureType> getSpawnTypes(Entity entity) {
-		List<EnumCreatureType> list = typesMap.get(entity.getClass());
-		if (list == null) {
-			list = Lists.newArrayList();
-			for (BiomeGenBase biome : BiomeGenBase.getBiomeGenArray()) {
-				if (biome != null) {
-					for (EnumCreatureType type : EnumCreatureType.values()) {
-						if (list.contains(type)) continue;
-						List<BiomeGenBase.SpawnListEntry> spawnableList = biome.getSpawnableList(type);
-						if (spawnableList != null) {
-							for (BiomeGenBase.SpawnListEntry entry : spawnableList) {
-								if (entry.entityClass == entity.getClass()) {
-									list.add(type);
-									break;
-								}
-							}
-						}
-					}
-				}
-			}
-			typesMap.put(entity.getClass(), list);
-		}
-		return list;
 	}
 
 	@SubscribeEvent
