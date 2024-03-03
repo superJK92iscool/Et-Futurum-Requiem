b50d43777ee6fe881e0b92c216f4c4cd61a795ca
Roadhog360 <Roadhog360@outlook.com>
2024-03-03 17:38:04 -0600

    Update BlockMossCarpet.java

--- src/main/java/ganymedes01/etfuturum/blocks/BlockMossCarpet.java
+++ src/main/java/ganymedes01/etfuturum/blocks/BlockMossCarpet.java
@@ -7,6 +7,7 @@ import ganymedes01.etfuturum.client.sound.ModSounds;
 import ganymedes01.etfuturum.core.utils.Utils;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.init.Blocks;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
@@ -66,6 +67,6 @@ public class BlockMossCarpet extends BaseBlock {
 	@SideOnly(Side.CLIENT)
 	public boolean shouldSideBeRendered(IBlockAccess p_149646_1_, int p_149646_2_, int p_149646_3_, int p_149646_4_, int p_149646_5_)
 	{
-		return p_149646_5_ == 1 || super.shouldSideBeRendered(p_149646_1_, p_149646_2_, p_149646_3_, p_149646_4_, p_149646_5_);
+		return Blocks.carpet.shouldSideBeRendered(p_149646_1_, p_149646_2_, p_149646_3_, p_149646_4_, p_149646_5_);
 	}
 }
