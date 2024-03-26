c7b959c9402f975b539cdf85066897a2ff9c15af
Roadhog360 <Roadhog360@outlook.com>
2024-03-26 15:56:10 -0500

    Disable debug code, slight fixes

--- src/main/java/ganymedes01/etfuturum/world/EtFuturumLateWorldGenerator.java
+++ src/main/java/ganymedes01/etfuturum/world/EtFuturumLateWorldGenerator.java
@@ -129,15 +129,13 @@ public class EtFuturumLateWorldGenerator extends EtFuturumWorldGenerator {
 		final int chunkMultiplierX = chunk.xPosition << 4;
 		final int chunkMultiplierZ = chunk.zPosition << 4;
 
-		for (int y = 0; y <= ConfigWorld.deepslateMaxY; y++) {
+		for (int y = 0; y <= Math.min(ConfigWorld.deepslateMaxY, chunk.worldObj.getHeight()); y++) {
+			ExtendedBlockStorage array = chunk.getBlockStorageArray()[y >> 4];
 			for (int x = 0; x < 16; x++) {
 				for (int z = 0; z < 16; z++) {
 					if (ConfigWorld.deepslateMaxY >= 255 || y < ConfigWorld.deepslateMaxY - 4 || y <= ConfigWorld.deepslateMaxY - chunk.worldObj.rand.nextInt(4)) {
-
 						worldX = x + chunkMultiplierX;
 						worldZ = z + chunkMultiplierZ;
-
-						ExtendedBlockStorage array = chunk.getBlockStorageArray()[y >> 4];
 
 						if (array == null) continue;
 
