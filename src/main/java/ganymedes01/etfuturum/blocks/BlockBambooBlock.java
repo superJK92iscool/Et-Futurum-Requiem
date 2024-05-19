32bb4eaf8f356822c209b1f47e9090aa18b13376
Kam <KamronGaming@gmail.com>
2024-05-19 14:07:27 -0400

    Revert "UTD Compat"
    This reverts commit 65c48e0bafc90773792cc46d159db176794fa45a.

--- src/main/java/ganymedes01/etfuturum/blocks/BlockBambooBlock.java
+++ src/main/java/ganymedes01/etfuturum/blocks/BlockBambooBlock.java
@@ -3,9 +3,7 @@ package ganymedes01.etfuturum.blocks;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import ganymedes01.etfuturum.client.sound.ModSounds;
-import ganymedes01.etfuturum.compat.ModsList;
 import ganymedes01.etfuturum.configuration.configs.ConfigBlocksItems;
-import ganymedes01.etfuturum.configuration.configs.ConfigModCompat;
 import ganymedes01.etfuturum.core.utils.Utils;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
@@ -28,7 +26,7 @@ public class BlockBambooBlock extends BaseLog {
 	@Override
 	public void getSubBlocks(Item p_149666_1_, CreativeTabs p_149666_2_, List p_149666_3_) {
 		p_149666_3_.add(new ItemStack(p_149666_1_, 1, 0));
-		if (ConfigBlocksItems.enableStrippedLogs || (ModsList.UPTODATE.isLoaded() && ConfigModCompat.upToDateCompat)) {
+		if (ConfigBlocksItems.enableStrippedLogs) {
 			p_149666_3_.add(new ItemStack(p_149666_1_, 1, 1));
 		}
 	}
