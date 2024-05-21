dd8c04f004e0c106c6b7a10fb08f416511e1a143
Roadhog360 <Roadhog360@outlook.com>
2024-05-21 15:22:56 -0500

    Unconditionally register renderers

--- src/main/java/ganymedes01/etfuturum/core/proxy/ClientProxy.java
+++ src/main/java/ganymedes01/etfuturum/core/proxy/ClientProxy.java
@@ -14,8 +14,6 @@ import ganymedes01.etfuturum.client.renderer.tileentity.*;
 import ganymedes01.etfuturum.client.skins.NewRenderPlayer;
 import ganymedes01.etfuturum.client.skins.NewSkinManager;
 import ganymedes01.etfuturum.client.subtitle.GuiSubtitles;
-import ganymedes01.etfuturum.configuration.configs.ConfigBlocksItems;
-import ganymedes01.etfuturum.configuration.configs.ConfigEntities;
 import ganymedes01.etfuturum.configuration.configs.ConfigFunctions;
 import ganymedes01.etfuturum.configuration.configs.ConfigMixins;
 import ganymedes01.etfuturum.core.handlers.ClientEventHandler;
@@ -75,182 +73,73 @@ public class ClientProxy extends CommonProxy {
 	}
 
 	private void registerItemRenderers() {
-		if (ConfigBlocksItems.enableBanners) {
-			MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(ModBlocks.BANNER.get()), new ItemBannerRenderer());
-		}
-		if (ConfigFunctions.enableFancySkulls) {
-			MinecraftForgeClient.registerItemRenderer(Items.skull, new ItemSkullRenderer());
-		}
-		if (ConfigFunctions.enableBowRendering) {
-			MinecraftForgeClient.registerItemRenderer(Items.bow, new ItemBowRenderer());
-		}
-		if (ConfigBlocksItems.enableShulkerBoxes) {
-			MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(ModBlocks.SHULKER_BOX.get()), new ItemShulkerBoxRenderer());
-		}
-		if (ConfigFunctions.inventoryBedModels) {
-			MinecraftForgeClient.registerItemRenderer(Items.bed, new Item3DBedRenderer((BlockBed) Blocks.bed));
-			for (ModBlocks bed : ModBlocks.BEDS) {
-				if (bed.isEnabled()) {
-					MinecraftForgeClient.registerItemRenderer(bed.getItem(), new Item3DBedRenderer((BlockBed) bed.get()));
-				}
+		MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(ModBlocks.BANNER.get()), new ItemBannerRenderer());
+		MinecraftForgeClient.registerItemRenderer(Items.skull, new ItemSkullRenderer());
+		MinecraftForgeClient.registerItemRenderer(Items.bow, new ItemBowRenderer());
+		MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(ModBlocks.SHULKER_BOX.get()), new ItemShulkerBoxRenderer());
+		MinecraftForgeClient.registerItemRenderer(Items.bed, new Item3DBedRenderer((BlockBed) Blocks.bed));
+		for (ModBlocks bed : ModBlocks.BEDS) {
+			if (bed.isEnabled()) {
+				MinecraftForgeClient.registerItemRenderer(bed.getItem(), new Item3DBedRenderer((BlockBed) bed.get()));
 			}
 		}
 	}
 
 	private void registerBlockRenderers() {
+		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityWoodSign.class, new TileEntityWoodSignRenderer());
+		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityBanner.class, new TileEntityBannerRenderer());
+		ClientRegistry.bindTileEntitySpecialRenderer(TileEntitySkull.class, new TileEntityFancySkullRenderer());
+		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityNewBeacon.class, new TileEntityNewBeaconRenderer());
+		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityShulkerBox.class, new TileEntityShulkerBoxRenderer(new ModelShulker()));
+		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityGateway.class, new TileEntityGatewayRenderer());
+
 		RenderingRegistry.registerBlockHandler(new BlockExtendedCrossedSquaresRenderer());
 		RenderingRegistry.registerBlockHandler(new BlockNewFenceRenderer());
 		RenderingRegistry.registerBlockHandler(new BlockTrapDoorRenderer(RenderIDs.TRAPDOOR));
 		RenderingRegistry.registerBlockHandler(new BlockDoorRenderer(RenderIDs.DOOR));
-		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityWoodSign.class, new TileEntityWoodSignRenderer());
-
-		if (ModBlocks.SLIME.isEnabled()) {
-			RenderingRegistry.registerBlockHandler(new BlockDoubleCubeRenderer(13, RenderIDs.SLIME_BLOCK));
-		}
-		
-		if (ModBlocks.BANNER.isEnabled()) {
-			ClientRegistry.bindTileEntitySpecialRenderer(TileEntityBanner.class, new TileEntityBannerRenderer());
-		}
-
-		if (ConfigFunctions.enableFancySkulls) {
-			ClientRegistry.bindTileEntitySpecialRenderer(TileEntitySkull.class, new TileEntityFancySkullRenderer());
-		}
-
-		if (ModBlocks.END_ROD.isEnabled()) {
-			RenderingRegistry.registerBlockHandler(new BlockEndRodRenderer(RenderIDs.END_ROD));
-		}
-
-		if (ModBlocks.CHORUS_FLOWER.isEnabled()) {
-			RenderingRegistry.registerBlockHandler(new BlockChorusFlowerRenderer(RenderIDs.CHORUS_FLOWER));
-		}
-
-		if (ModBlocks.CHORUS_PLANT.isEnabled()) {
-			RenderingRegistry.registerBlockHandler(new BlockChorusPlantRenderer(RenderIDs.CHORUS_PLANT));
-		}
-
-		if (ModBlocks.BEACON.isEnabled()) {
-			ClientRegistry.bindTileEntitySpecialRenderer(TileEntityNewBeacon.class, new TileEntityNewBeaconRenderer());
-		}
-
-		if (ModBlocks.LANTERN.isEnabled()) {
-			RenderingRegistry.registerBlockHandler(new BlockLanternRenderer(RenderIDs.LANTERN));
-		}
-
-		if (ModBlocks.BARREL.isEnabled()) {
-			RenderingRegistry.registerBlockHandler(new BlockBarrelRenderer(RenderIDs.BARREL));
-		}
-
-		if (ConfigBlocksItems.enableGlazedTerracotta) {
-			RenderingRegistry.registerBlockHandler(new BlockGlazedTerracottaRenderer(RenderIDs.GLAZED_TERRACOTTA));
-		}
-
-		if (ModBlocks.LAVA_CAULDRON.isEnabled()) {
-			RenderingRegistry.registerBlockHandler(new BlockLavaCauldronRenderer(RenderIDs.LAVA_CAULDRON));
-		}
-
-		if (ModBlocks.SHULKER_BOX.isEnabled()) {
-			ClientRegistry.bindTileEntitySpecialRenderer(TileEntityShulkerBox.class, new TileEntityShulkerBoxRenderer(new ModelShulker()));
-		}
-
-		if (ModBlocks.STONECUTTER.isEnabled()) {
-			RenderingRegistry.registerBlockHandler(new BlockStonecutterRenderer(RenderIDs.STONECUTTER));
-		}
-
-		if (ModBlocks.COMPOSTER.isEnabled()) {
-			RenderingRegistry.registerBlockHandler(new BlockComposterRenderer(RenderIDs.COMPOSTER));
-		}
-
-		if (ModBlocks.LOOM.isEnabled()) {
-			RenderingRegistry.registerBlockHandler(new BlockLoomRenderer(RenderIDs.LOOM));
-		}
-
-		if (ModBlocks.AMETHYST_CLUSTER_1.isEnabled() && ModBlocks.AMETHYST_CLUSTER_2.isEnabled()) {
-			RenderingRegistry.registerBlockHandler(new BlockAmethystClusterRenderer(RenderIDs.AMETHYST_CLUSTER));
-		}
-
-		if (ModBlocks.POINTED_DRIPSTONE.isEnabled()) {
-			RenderingRegistry.registerBlockHandler(new BlockPointedDripstoneRenderer(RenderIDs.POINTED_DRIPSTONE));
-		}
-
+		RenderingRegistry.registerBlockHandler(new BlockDoubleCubeRenderer(13, RenderIDs.SLIME_BLOCK));
+		RenderingRegistry.registerBlockHandler(new BlockEndRodRenderer(RenderIDs.END_ROD));
+		RenderingRegistry.registerBlockHandler(new BlockChorusFlowerRenderer(RenderIDs.CHORUS_FLOWER));
+		RenderingRegistry.registerBlockHandler(new BlockChorusPlantRenderer(RenderIDs.CHORUS_PLANT));
+		RenderingRegistry.registerBlockHandler(new BlockLanternRenderer(RenderIDs.LANTERN));
+		RenderingRegistry.registerBlockHandler(new BlockBarrelRenderer(RenderIDs.BARREL));
+		RenderingRegistry.registerBlockHandler(new BlockGlazedTerracottaRenderer(RenderIDs.GLAZED_TERRACOTTA));
+		RenderingRegistry.registerBlockHandler(new BlockLavaCauldronRenderer(RenderIDs.LAVA_CAULDRON));
+		RenderingRegistry.registerBlockHandler(new BlockStonecutterRenderer(RenderIDs.STONECUTTER));
+		RenderingRegistry.registerBlockHandler(new BlockComposterRenderer(RenderIDs.COMPOSTER));
+		RenderingRegistry.registerBlockHandler(new BlockLoomRenderer(RenderIDs.LOOM));
+		RenderingRegistry.registerBlockHandler(new BlockAmethystClusterRenderer(RenderIDs.AMETHYST_CLUSTER));
+		RenderingRegistry.registerBlockHandler(new BlockPointedDripstoneRenderer(RenderIDs.POINTED_DRIPSTONE));
 		RenderingRegistry.registerBlockHandler(new BlockColoredWaterCauldronRenderer(RenderIDs.COLORED_CAULDRON));
-
 		RenderingRegistry.registerBlockHandler(new BlockChestRenderer());
-
-		if (ModBlocks.OBSERVER.isEnabled()) {
-			RenderingRegistry.registerBlockHandler(new BlockObserverRenderer(RenderIDs.OBSERVER));
-		}
-
-		if (ModBlocks.END_GATEWAY.isEnabled()) {
-			ClientRegistry.bindTileEntitySpecialRenderer(TileEntityGateway.class, new TileEntityGatewayRenderer());
-		}
-
-		if (ModBlocks.HONEY_BLOCK.isEnabled()) {
-			RenderingRegistry.registerBlockHandler(new BlockDoubleCubeRenderer(15, RenderIDs.HONEY_BLOCK));
-		}
-
-		if (ModBlocks.CHAIN.isEnabled()) {
-			RenderingRegistry.registerBlockHandler(new BlockChainRenderer(RenderIDs.CHAIN));
-		}
-
-		if(ModBlocks.AZALEA.isEnabled()){
-			RenderingRegistry.registerBlockHandler(new BlockAzaleaRenderer(RenderIDs.AZALEA));
-		}
-
-		if (ModBlocks.MANGROVE_ROOTS.isEnabled()) {
-			RenderingRegistry.registerBlockHandler(new BlockMangroveRootsRenderer(RenderIDs.MANGROVE_ROOTS));
-		}
-
-		if (ModBlocks.PINK_PETALS.isEnabled()) {
-			RenderingRegistry.registerBlockHandler(new BlockPinkPetalsRenderer(RenderIDs.PINK_PETALS));
-		}
-
-		if (ModBlocks.BAMBOO.isEnabled()) {
-			RenderingRegistry.registerBlockHandler(new BlockBambooRenderer(RenderIDs.BAMBOO));
-		}
+		RenderingRegistry.registerBlockHandler(new BlockObserverRenderer(RenderIDs.OBSERVER));
+		RenderingRegistry.registerBlockHandler(new BlockDoubleCubeRenderer(15, RenderIDs.HONEY_BLOCK));
+		RenderingRegistry.registerBlockHandler(new BlockChainRenderer(RenderIDs.CHAIN));
+		RenderingRegistry.registerBlockHandler(new BlockAzaleaRenderer(RenderIDs.AZALEA));
+		RenderingRegistry.registerBlockHandler(new BlockMangroveRootsRenderer(RenderIDs.MANGROVE_ROOTS));
+		RenderingRegistry.registerBlockHandler(new BlockPinkPetalsRenderer(RenderIDs.PINK_PETALS));
+		RenderingRegistry.registerBlockHandler(new BlockBambooRenderer(RenderIDs.BAMBOO));
 
 		RenderingRegistry.registerBlockHandler(new BlockEmissiveLayerRenderer(RenderIDs.EMISSIVE_DOUBLE_LAYER));
 	}
 
 	private void registerEntityRenderers() {
-		if (ConfigBlocksItems.enableArmourStand)
-			RenderingRegistry.registerEntityRenderingHandler(EntityArmourStand.class, new ArmourStandRenderer());
-		if (ConfigEntities.enableEndermite)
-			RenderingRegistry.registerEntityRenderingHandler(EntityEndermite.class, new EndermiteRenderer());
-		if (ConfigEntities.enableRabbit)
-			RenderingRegistry.registerEntityRenderingHandler(EntityRabbit.class, new RabbitRenderer());
-
-		if (ConfigEntities.enableHusk)
-			RenderingRegistry.registerEntityRenderingHandler(EntityHusk.class, new HuskRenderer());
-		if (ConfigEntities.enableStray) {
-			RenderingRegistry.registerEntityRenderingHandler(EntityStray.class, new StrayRenderer());
-		}
-
-		if (ConfigBlocksItems.enableLingeringPotions) {
-			RenderingRegistry.registerEntityRenderingHandler(EntityLingeringPotion.class, new LingeringPotionRenderer());
-			RenderingRegistry.registerEntityRenderingHandler(EntityLingeringEffect.class, new LingeringEffectRenderer());
-		}
-		if (ConfigEntities.enableVillagerZombies)
-			RenderingRegistry.registerEntityRenderingHandler(EntityZombieVillager.class, new VillagerZombieRenderer());
-
-		if (ConfigEntities.enableDragonRespawn)
-			RenderingRegistry.registerEntityRenderingHandler(EntityPlacedEndCrystal.class, new PlacedEndCrystalRenderer());
-
-		if (ConfigEntities.enableBrownMooshroom)
-			RenderingRegistry.registerEntityRenderingHandler(EntityBrownMooshroom.class, new BrownMooshroomRenderer());
-
-		if (ConfigBlocksItems.enableNewBoats) {
-			RenderingRegistry.registerEntityRenderingHandler(EntityNewBoat.class, new NewBoatRenderer());
-			RenderingRegistry.registerEntityRenderingHandler(EntityNewBoatWithChest.class, new ChestBoatRenderer());
-		}
-
-		if (ConfigEntities.enableShulker) {
-			RenderingRegistry.registerEntityRenderingHandler(EntityShulker.class, new ShulkerRenderer());
-			RenderingRegistry.registerEntityRenderingHandler(EntityShulkerBullet.class, new ShulkerBulletRenderer());
-		}
-
-		if (ConfigEntities.enableBees) {
-			RenderingRegistry.registerEntityRenderingHandler(EntityBee.class, new BeeRenderer());
-		}
+		RenderingRegistry.registerEntityRenderingHandler(EntityArmourStand.class, new ArmourStandRenderer());
+		RenderingRegistry.registerEntityRenderingHandler(EntityEndermite.class, new EndermiteRenderer());
+		RenderingRegistry.registerEntityRenderingHandler(EntityRabbit.class, new RabbitRenderer());
+		RenderingRegistry.registerEntityRenderingHandler(EntityHusk.class, new HuskRenderer());
+		RenderingRegistry.registerEntityRenderingHandler(EntityStray.class, new StrayRenderer());
+		RenderingRegistry.registerEntityRenderingHandler(EntityLingeringPotion.class, new LingeringPotionRenderer());
+		RenderingRegistry.registerEntityRenderingHandler(EntityLingeringEffect.class, new LingeringEffectRenderer());
+		RenderingRegistry.registerEntityRenderingHandler(EntityZombieVillager.class, new VillagerZombieRenderer());
+		RenderingRegistry.registerEntityRenderingHandler(EntityPlacedEndCrystal.class, new PlacedEndCrystalRenderer());
+		RenderingRegistry.registerEntityRenderingHandler(EntityBrownMooshroom.class, new BrownMooshroomRenderer());
+		RenderingRegistry.registerEntityRenderingHandler(EntityNewBoat.class, new NewBoatRenderer());
+		RenderingRegistry.registerEntityRenderingHandler(EntityNewBoatWithChest.class, new ChestBoatRenderer());
+		RenderingRegistry.registerEntityRenderingHandler(EntityShulker.class, new ShulkerRenderer());
+		RenderingRegistry.registerEntityRenderingHandler(EntityShulkerBullet.class, new ShulkerBulletRenderer());
+		RenderingRegistry.registerEntityRenderingHandler(EntityBee.class, new BeeRenderer());
+		RenderingRegistry.registerEntityRenderingHandler(EntityNewSnowGolem.class, new NewSnowGolemRenderer());
 
 		RenderingRegistry.registerEntityRenderingHandler(EntityPig.class, new TechnobladeCrownRenderer());
 
@@ -266,7 +155,5 @@ public class ClientProxy extends CommonProxy {
 
 			RenderManager.instance.entityRenderMap.put(EntityPlayer.class, new NewRenderPlayer());
 		}
-		if (ConfigEntities.enableShearableSnowGolems)
-			RenderingRegistry.registerEntityRenderingHandler(EntityNewSnowGolem.class, new NewSnowGolemRenderer());
 	}
 }
