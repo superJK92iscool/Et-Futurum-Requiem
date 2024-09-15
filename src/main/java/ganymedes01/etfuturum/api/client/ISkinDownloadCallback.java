0989b2b0f2f159c8ebd4c919c4ed76bef3563ed6
roadhog360 <roadhog360@outlook.com>
2024-09-14 22:00:36 -0500

    Mark this useless interface as deprecated, make early mixin config conformVisibility = true

--- src/main/java/ganymedes01/etfuturum/api/client/ISkinDownloadCallback.java
+++ src/main/java/ganymedes01/etfuturum/api/client/ISkinDownloadCallback.java
@@ -7,13 +7,17 @@ import net.minecraft.client.resources.SkinManager.SkinAvailableCallback;
 import net.minecraft.util.ResourceLocation;
 
 @SideOnly(Side.CLIENT)
+@Deprecated
 public interface ISkinDownloadCallback extends SkinAvailableCallback {
 
 	/**
 	 * Gets called when a 1.8 style skin is downloaded
 	 * <p>
+	 * No longer used. Kept around to not break mods that might call this.
+	 * Not sure what it was ever used for, since it's not implemented on anything.
 	 * MCP name: {@code onSkinAvailable}
 	 */
 	@Override
+	@Deprecated
 	void func_152121_a(Type skinType, ResourceLocation resourceLocation);
 }
