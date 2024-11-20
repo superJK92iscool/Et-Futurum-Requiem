950730c76aa7b82f2e7b246e23b481118bb7b395
Robert <Roadhog360@outlook.com>
2024-11-20 12:51:06 -0600

    Fix RPLE compat requiring all compatible blocks enabled

--- src/main/java/ganymedes01/etfuturum/compat/CompatRPLEEventHandler.java
+++ src/main/java/ganymedes01/etfuturum/compat/CompatRPLEEventHandler.java
@@ -5,7 +5,9 @@ import com.falsepattern.rple.api.common.event.BlockColorRegistrationEvent;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import ganymedes01.etfuturum.ModBlocks;
+import ganymedes01.etfuturum.recipes.ModRecipes;
 import net.minecraft.block.Block;
+import ganymedes01.etfuturum.recipes.ModRecipes;
 
 import static com.falsepattern.rple.api.common.color.DefaultColor.*;
 
@@ -21,17 +23,17 @@ public class CompatRPLEEventHandler {
 	public void kolorsLol(BlockColorRegistrationEvent event) {
 		RPLEBlockColorRegistry registry = event.registry();
 
-		registry.colorizeBlock(ModBlocks.SEA_LANTERN.get()).brightness(0xAFF).apply();
-		registry.colorizeBlock(ModBlocks.END_ROD.get()).brightness(0xFAF).apply();
-		registry.colorizeBlock(ModBlocks.MAGMA.get()).brightness(0x510).apply();
-		registry.colorizeBlock(ModBlocks.BEACON.get()).brightness(0xCCC).apply();
-		registry.colorizeBlock(ModBlocks.DEEPSLATE_LIT_REDSTONE_ORE.get()).brightness(0x511).apply();
-		registry.colorizeBlock(ModBlocks.LANTERN.get()).brightness(0xFC5).apply();
-		registry.colorizeBlock(ModBlocks.SOUL_LANTERN.get()).brightness(0x49A).apply();
-		registry.colorizeBlock(ModBlocks.SOUL_TORCH.get()).brightness(0x49A).apply();
-		registry.colorizeBlock(ModBlocks.SHROOMLIGHT.get()).brightness(0xFC8).apply();
-		registry.colorizeBlock(ModBlocks.LIT_BLAST_FURNACE.get()).brightness(0xDCA).apply();
-		registry.colorizeBlock(ModBlocks.LIT_SMOKER.get()).brightness(0xDCA).apply();
+		colorize(registry, ModBlocks.SEA_LANTERN.get(), 0xAFF);
+		colorize(registry, ModBlocks.END_ROD.get(), 0xFAF);
+		colorize(registry, ModBlocks.MAGMA.get(), 0x510);
+		colorize(registry, ModBlocks.BEACON.get(), 0xCCC);
+		colorize(registry, ModBlocks.DEEPSLATE_LIT_REDSTONE_ORE.get(), 0x511);
+		colorize(registry, ModBlocks.LANTERN.get(), 0xFC5);
+		colorize(registry, ModBlocks.SOUL_LANTERN.get(), 0x49A);
+		colorize(registry, ModBlocks.SOUL_TORCH.get(), 0x49A);
+		colorize(registry, ModBlocks.SHROOMLIGHT.get(), 0xFC8);
+		colorize(registry, ModBlocks.LIT_BLAST_FURNACE.get(), 0xDCA);
+		colorize(registry, ModBlocks.LIT_SMOKER.get(), 0xDCA);
 
 		registerAmethystColor(registry, 0, 0x101);
 		registerAmethystColor(registry, 1, 0x212);
@@ -43,22 +45,46 @@ public class CompatRPLEEventHandler {
 		registerBulbColor(registry, 2, 0x863);
 		registerBulbColor(registry, 3, 0x443);
 
-		registry.colorizeBlock(ModBlocks.SLIME.get()).translucency(LIME).apply();
-		registry.colorizeBlock(ModBlocks.HONEY_BLOCK.get()).translucency(0xFB7).apply();
+		transparency(registry, ModBlocks.SLIME.get(), LIME.rgb16());
+		transparency(registry, ModBlocks.HONEY_BLOCK.get(), 0xFB7);
 	}
 
 	private void registerAmethystColor(RPLEBlockColorRegistry registry, int clusterSize, int color) {
 		Block cluster = clusterSize < 2 ? ModBlocks.AMETHYST_CLUSTER_1.get() : ModBlocks.AMETHYST_CLUSTER_2.get();
 		boolean secondHalf = clusterSize % 2 == 0;
 		for(int i = secondHalf ? 0 : 6; i < (secondHalf ? 6 : 12); i++) {
-			registry.colorizeBlock(cluster, i).brightness(color).apply();
+			colorize(registry, cluster, i, color);
 		}
 	}
 
 	private void registerBulbColor(RPLEBlockColorRegistry registry, int level, int color) {
-		registry.colorizeBlock(ModBlocks.COPPER_BULB.get(), level + 4).brightness(color).apply();
-		registry.colorizeBlock(ModBlocks.COPPER_BULB.get(), level + 12).brightness(color).apply();
-		registry.colorizeBlock(ModBlocks.POWERED_COPPER_BULB.get(), level + 4).brightness(color).apply();
-		registry.colorizeBlock(ModBlocks.POWERED_COPPER_BULB.get(), level + 12).brightness(color).apply();
+		colorize(registry, ModBlocks.COPPER_BULB.get(), level + 4, color);
+		colorize(registry, ModBlocks.COPPER_BULB.get(), level + 12, color);
+		colorize(registry, ModBlocks.POWERED_COPPER_BULB.get(), level + 4, color);
+		colorize(registry, ModBlocks.POWERED_COPPER_BULB.get(), level + 12, color);
+	}
+	
+	private void colorize(RPLEBlockColorRegistry registry, Block block, int rgb16) {
+		if(ModRecipes.validateItems(block)) {
+			registry.colorizeBlock(block).brightness(rgb16).apply();
+		}
+	}
+
+	private void colorize(RPLEBlockColorRegistry registry, Block block, int meta, int rgb16) {
+		if(ModRecipes.validateItems(block)) {
+			registry.colorizeBlock(block, meta).brightness(rgb16).apply();
+		}
+	}
+
+	private void transparency(RPLEBlockColorRegistry registry, Block block, int rgb16) {
+		if(ModRecipes.validateItems(block)) {
+			registry.colorizeBlock(block).translucency(rgb16).apply();
+		}
+	}
+
+	private void transparency(RPLEBlockColorRegistry registry, Block block, int meta, int rgb16) {
+		if(ModRecipes.validateItems(block)) {
+			registry.colorizeBlock(block, meta).translucency(rgb16).apply();
+		}
 	}
 }
